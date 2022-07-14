fd048d2e24f3de0d89ba6694ff49705923770337
shxtrk <direct@shxtrk.dev>
2022-07-14 16:52:51 +0300

    2236. Root Equals Sum of Children

---
+++ c/RootEqualsSumOfChildren.c
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/root-equals-sum-of-children/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool checkTree(struct TreeNode* root){
+    if (root->val == root->left->val + root->right->val) {
+        return true;
+    }
+    return false;
+}
