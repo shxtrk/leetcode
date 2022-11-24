a5cbbfc7f25b317f58f384dd386365ce70014c9a
shxtrk <direct@shxtrk.dev>
2022-11-24 21:48:19 +0200

    110. Balanced Binary Tree

---
+++ swift/BalancedBinaryTree.swift
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/balanced-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+ 
+class Solution {
+
+    func isBalanced(_ root: TreeNode?) -> Bool {
+
+        var isBalanced = true
+
+        func height(_ root: TreeNode?) -> Int {
+            guard root != nil else { return 0 }
+            let leftHeight = height(root!.left)
+            let rightHeight = height(root!.right)
+            if abs(leftHeight - rightHeight) > 1 {
+                isBalanced = false
+            }
+            return max(leftHeight, rightHeight) + 1
+        }
+
+        height(root)
+        return isBalanced
+    }
+}
