10ea9aa4975be5445eb842a9edbe4abd054374b8
Serhii Striuk <66003286+shxtrk@users.noreply.github.com>
2025-02-15 17:12:34 +0200

    88. Merge Sorted Array

--- swift/MergeSortedArray.swift
+++ swift/MergeSortedArray.swift
@@ -2,6 +2,20 @@
 
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        nums1 = (Array(nums1[0..<m]) + nums2).sorted { $0 < $1 }
+        var p1 = 0
+        var p2 = 0
+        var counter = 0
+
+        let nums1c = nums1[0..<m]
+
+        for i in 0..<m+n {
+            if p2 >= n || (p1 < m && nums1c[p1] < nums2[p2])  {
+                nums1[i] = nums1c[p1]
+                p1 += 1
+            } else if p2 < n {
+                nums1[i] = nums2[p2]
+                p2 += 1
+            }
+        }
     }
 }
