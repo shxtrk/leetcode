141e5ac85efaebe76cc77a8d53b55b547491e467
Serhii Striuk <66003286+shxtrk@users.noreply.github.com>
2025-02-18 14:37:38 +0200

    383. Ransom Note

--- swift/RansomNote.swift
+++ swift/RansomNote.swift
@@ -2,15 +2,14 @@
 
 class Solution {
     func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
-        var table = [Character: Int]()
-        for c in magazine {
-            table[c, default: 0] += 1
+        var dict = [Character: Int]()
+        magazine.forEach {
+            dict[$0, default: 0] += 1
         }
-        for c in ransomNote {
-            table[c, default: 0] -= 1
-        }
-        for (i, n) in table {
-            if n<0 {
+        for i in ransomNote {
+            if let count = dict[i], count > 0 {
+                dict[i] = count - 1
+            } else {
                 return false
             }
         }
